8cd55c181295b1685f5d6312b44d7ee68c731fa8
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-19 22:30:02 +0300

    Пример разобранный

---
+++ SECOND/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+int result = number * number;
+Console.WriteLine($"квадрат числа {number} = {result}");
+Console.WriteLine("квадрат числа "+number+ " = " +result);
+Console.WriteLine("квадрат числа {0} = {1}", number, result);
+Console.WriteLine("квадрат числа {number} = {result}");
