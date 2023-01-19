a4b6c048b0dd9ef75e1f3927eeb49380e5dfcf70
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-20 00:12:09 +0300

    Пример_2_итог

---
+++ Primer_2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+if (number == 1)
+{
+    Console.WriteLine("День недели понедельник");
+}
+else if (number == 2)
+{
+    Console.WriteLine("День недели вторник");
+}
+else if (number == 3)
+{
+    Console.WriteLine("День недели среда");
+}
+else if (number == 4)
+{
+    Console.WriteLine("День недели четверг");
+}
+else if (number == 5)
+{
+    Console.WriteLine("День недели пятница");
+}
+else if (number == 6)
+{
+    Console.WriteLine("День недели суббота");
+}
+else if (number == 7)
+{
+    Console.WriteLine("День недели воскресенье");
+}
+else 
+{
+    Console.WriteLine("Такого дня нет");
+}
