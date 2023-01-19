3fac1da9c61a5ac18a5f792407f53318d87e3275
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-20 00:48:35 +0300

    Пример_3_итог

---
+++ Primer_3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+for (int i = - number; i <= number; i++)
+{
+    Console.Write(i + ",");
+}
