5f4b8c78c8fbdd3cbf55bfed79717e486eb32d98
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-20 02:21:15 +0300

    DZ_1_n_1

---
+++ DZ_1_n_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+string numberStr1 = Console.ReadLine();
+int a = Convert.ToInt32(numberStr1);
+Console.WriteLine("Введите второе число");
+string numberStr2 = Console.ReadLine();
+int b = Convert.ToInt32(numberStr2);
+if (a>b)
+{
+    Console.WriteLine($"max={a}, min={b}");
+}
+else
+{
+    Console.WriteLine($"max={b}, min={a}");
+}
