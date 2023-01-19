32c39eeb172f41252c52547ce6280979446d0db6
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-19 23:11:31 +0300

    Пример_1

---
+++ Primer_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+string numberStr1 = Console.ReadLine();
+int a = Convert.ToInt32(numberStr1);
+Console.WriteLine("Введите второе число");
+string numberStr2 = Console.ReadLine();
+int b = Convert.ToInt32(numberStr2);
+int c = a*a;
+if (c == b)
+{
+    Console.WriteLine("Первое число квадрат второго");
+    }
+    else
+    {
+     Console.WriteLine("Первое число не является квадратом второго");
+    }
