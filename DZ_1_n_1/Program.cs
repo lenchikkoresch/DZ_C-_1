be81057f1e7bb4d41cd7c99627ea71be86f1b39a
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-20 02:44:57 +0300

    DZ_1_n_1_итог

--- DZ_1_n_1/Program.cs
+++ DZ_1_n_1/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine("Введите первое число");
+﻿/*Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7;
+a = 2; b = 10 -> max = 10;
+a = -9; b = -3 -> max = -3;
+*/
+
+
+Console.WriteLine("Введите первое число");
 string numberStr1 = Console.ReadLine();
 int a = Convert.ToInt32(numberStr1);
 Console.WriteLine("Введите второе число");
