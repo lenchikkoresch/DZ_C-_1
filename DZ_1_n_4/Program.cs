20bacaf6f4b7849c7470e48def413ca22de49e39
lenschik.kor <lenschik.kor@yandex.ru>
2023-01-20 04:00:43 +0300

    DZ_1_n_4

---
+++ DZ_1_n_4/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int a = Convert.ToInt32(numberStr);
+for (int i = 1; i<=a; i++)
+{
+    int c=i % 2;
+    if (c == 0)
+    {
+      Console.Write(i + ",");  
+    }
+}
